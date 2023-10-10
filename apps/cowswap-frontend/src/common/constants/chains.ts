6b2992f83bc9f96be98d7472ead8d9449cf94517
MANIAHMED <abdulmani11111111@gmail.com>
2023-10-10 18:56:33 +0500

    feat(addliquidity): supported chain ids added, function to verify supported chain id added

---
+++ apps/cowswap-frontend/src/common/constants/chains.ts
@@ -0,0 +1,26 @@
+export enum SupportedChainId {
+    MAINNET = 1,
+    GOERLI = 5,
+    SEPOLIA = 11155111,
+  
+    ARBITRUM_ONE = 42161,
+    ARBITRUM_GOERLI = 421613,
+  
+    OPTIMISM = 10,
+    OPTIMISM_GOERLI = 420,
+  
+    POLYGON = 137,
+    POLYGON_MUMBAI = 80001,
+  
+    CELO = 42220,
+    CELO_ALFAJORES = 44787,
+  
+    BNB = 56,
+    //changes
+    LIGHTLINK_PHOENIX_MAINNET = 1890,
+    LIGHTLINK_PEGASUS_TESTNET = 1891,
+  }
+  
+export function isSupportedChain(chainId: number | null | undefined): chainId is SupportedChainId {
+  return !!chainId && !!SupportedChainId[chainId]
+  }
