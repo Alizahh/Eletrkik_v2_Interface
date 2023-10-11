87d100facd3fbfcc70f673632b54524f7aba9a7f
MANIAHMED <abdulmani11111111@gmail.com>
2023-10-11 13:32:45 +0500

    feat(addliquidity): supportedchainid, unwrappedtoken function added

---
+++ apps/cowswap-frontend/src/modules/pools/utils/unwrappedToken.ts
@@ -0,0 +1,13 @@
+import { Currency } from '@uniswap/sdk-core'
+import { nativeOnChain } from '../../../../../../libs/common-const/src/tokens'
+import { WRAPPED_NATIVE_CURRENCY } from '../constants/tokens'
+import { supportedChainId } from './supportedChainId'
+
+
+export function unwrappedToken(currency: Currency): Currency {
+  if (currency.isNative) return currency
+  const formattedChainId = supportedChainId(currency.chainId)
+  if (formattedChainId && WRAPPED_NATIVE_CURRENCY[formattedChainId]?.equals(currency))
+    return nativeOnChain(currency.chainId)
+  return currency
+}
